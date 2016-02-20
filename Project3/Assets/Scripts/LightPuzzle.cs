f9e59aaafd54baf66d79d243272b7df24b6d8528
bergercm <bergercm@umich.edu>
2016-02-20 14:48:50 -0500

    initial light puzzle commit

---
+++ Project3/Assets/Scripts/LightPuzzle.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Example Puzzle: 
+//	Lights need to be 10011
+//	Start at 11111
+//First button toggles 1&3
+//Second button toggles 2&4
+//Last button toggles 1&2
+
+//flip 2&4
+//10101
+
+//flip 1&3 
+//00001
+
+//flip 1&2
+//11001
+
+//flip 2&4
+//10011
+
+public class LightPuzzle : MonoBehaviour {
+
+	public static LightPuzzle S;
+	public BitArray toggled_lights;
+	public BitArray levers;
+
+	void Awake(){
+		S = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		toggled_lights = new BitArray (5, true);
+		levers = new BitArray (3, false);
+	}
+	
+	// Update is called once per frame
+	//Listen for input from P2s controller to toggle the lights above the door
+	void Update () {
+		GetToggleInput ();
+	}
+
+	void GetToggleInput(){
+		if (Input.GetKeyDown (KeyCode.U)) {
+			HandleInput ('u');
+		} else if (Input.GetKeyDown(KeyCode.I)){
+			HandleInput('i');
+		} else if (Input.GetKeyDown(KeyCode.O)){ //The letter
+			HandleInput('o');
+		}
+	}
+
+	//This is the function that implements the logic of flipping lights
+	void HandleInput(char c){
+		switch(c){
+		case 'u':
+			FlipBits(new int[1] {0}, levers);
+			FlipBits(new int[2] {0, 2}, toggled_lights);
+			break;
+		case 'i':
+			FlipBits (new int[1] { 1 }, levers);
+			FlipBits(new int[2] {1, 3}, toggled_lights);
+			break;
+		case 'o':
+			FlipBits (new int[1] { 2 }, levers);
+			FlipBits(new int[2] {0, 1}, toggled_lights);
+			break;
+		default:
+			break;
+		}
+	}
+
+	//function used to flip the bits in the array
+	void FlipBits(int[] positions, BitArray ba){
+		foreach (int index in positions) {
+			if (ba [index]) {
+				ba [index] = false;
+			} else {
+				ba [index] = true;
+			}
+		}
+	}
+}
